5bf6e511841f6bebc60cca0a57649f73aa19f37f
Bruno Moreira <bruno.moreira@vtex.com.br>
2022-05-15 11:21:37 -0300

    Remove useless controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, CoreModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, CoreModule, ProductsModule],
 })
 export class AppModule {}
