26da3d27f863a5a4d10b7ba358a1fab349a55e6e
Bruno Moreira <bruno.moreira@vtex.com.br>
2022-05-15 23:42:48 -0300

    Add tests to Prisma Service

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 describe('PrismaService', () => {
@@ -14,5 +15,29 @@ describe('PrismaService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('onModuleInit', () => {
+    it('should connect to prisma', async () => {
+      const connect = jest.spyOn(service, '$connect');
+
+      await service.onModuleInit();
+
+      expect(connect).toHaveBeenCalled();
+    });
+  });
+
+  describe('enableShutdownHooks', () => {
+    it('should enable shutdown hooks', async () => {
+      const app = {
+        close: jest.fn(),
+      };
+
+      const on = jest.spyOn(service, '$on');
+
+      await service.enableShutdownHooks(app as any);
+
+      expect(on).toHaveBeenCalledWith('beforeExit', expect.any(Function));
+    });
   });
 });
